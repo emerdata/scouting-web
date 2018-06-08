ce7a42ca36364ccecebfc305bf290c314f4b92b5
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-06-09 00:27:32 +0100

    Added MDL template to home.html

--- webapp/views.py
+++ webapp/views.py
@@ -5,8 +5,12 @@ from webapp import app
 @app.route('/')
 def hello_world():
     name = request.args.get('name', None)
-    return render_template('hello.html', name=name)
+    return render_template('home.html', name=name)
 
 @app.route('/favicon.ico')
 def favicon_route():
     return send_from_directory('static', 'favicon.ico')
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
