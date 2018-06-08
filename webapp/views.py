b141a1f9da0e47a9bb67d423f74f6b078385fb2b
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-06-08 23:59:22 +0100

    Added favicon

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 from webapp import app
 
 @app.route('/')
 def hello_world():
-    return '<!DOCTYPE HTML><html><head></head><body><h1>Hello!</h1><p>Hello, World!</p></body></html>'
+    return '<!DOCTYPE HTML><html><head><title>Emerdata, or something</title></head><body><h1>Hello!</h1><p>Hello, World!</p></body></html>'
+
+@app.route('/favicon.ico')
+def favicon_route():
+    return send_from_directory('static', 'favicon.ico')
