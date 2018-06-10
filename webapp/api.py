8265632e4f137e5370e917a12da6905f2fec292b
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-06-11 00:59:50 +0100

    Fixed api.py error where parameter was 'method' instead of 'methods'

--- webapp/api.py
+++ webapp/api.py
@@ -6,30 +6,30 @@ from webapp import app
 def login():
     pass
 
-@app.route('/api/logout', method=['POST'])
+@app.route('/api/logout', methods=['POST'])
 def logout():
     pass
 
-@app.route('/api/teams', method=['GET'])
+@app.route('/api/teams', methods=['GET'])
 def teams():
     pass
 
-@app.route('/api/events', method=['GET'])
+@app.route('/api/events', methods=['GET'])
 def events():
     pass
 
-@app.route('/api/matches', method=['GET'])
+@app.route('/api/matches', methods=['GET'])
 def matches():
     pass
 
-@app.route('/api/team/<int:team_number>', method=['GET'])
+@app.route('/api/team/<int:team_number>', methods=['GET'])
 def team(team_number):
     pass
 
-@app.route('/api/event/<event_id>', method=['GET'])
+@app.route('/api/event/<event_id>', methods=['GET'])
 def event(event_id):
     pass
 
-@app.route('/api/match/<int:match_id>', method=['GET'])
+@app.route('/api/match/<int:match_id>', methods=['GET'])
 def math(match_id):
     pass
